c267cbecda1121ee97c644dd754155ab186e0d08
Scott Kennedy <skennedy84@gmail.com>
2018-04-05 13:29:43 -0400

    Replacing confusing calculations with named functions

--- src/app/board/components/board.ts
+++ src/app/board/components/board.ts
@@ -9,9 +9,9 @@ import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core
         stroke="black"
         [attr.width]="cellWidth"
         [attr.height]="cellHeight"
-        [attr.x]="cellWidth * (i % width) + 'px'"
-        [attr.y]="cellHeight * (i - (i % width)) / height + 'px'"
-        [attr.fill]="(cell ? '#4CAF50' : '#A1887F')"></rect>
+        [attr.x]="getXOffsetFromIndex(i) + 'px'"
+        [attr.y]="getYOffsetFromIndex(i) + 'px'"
+        [attr.fill]="(cell ? '#4CAF50' : '#fff')"></rect>
     </svg>
   `
 })
@@ -27,5 +27,13 @@ export class BoardComponent implements OnInit {
   ngOnInit() {
     this.cellWidth = 800 / this.width;
     this.cellHeight = 600 / this.height;
+  }
+
+  getXOffsetFromIndex(index) {
+    return this.cellWidth * (index % this.width);
+  }
+
+  getYOffsetFromIndex(index) {
+    return this.cellHeight * (index - index % this.width) / this.height;
   }
 }
